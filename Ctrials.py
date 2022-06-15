e16a139aeb36a91da445480ce1d05902de9af077
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-06-15 13:22:06 +0200

    Update Ctrials.py

--- Ctrials.py
+++ Ctrials.py
@@ -4,5 +4,5 @@ import pandas as pd
 conn = psycopg2.connect(dbname='aact', user='git0bf', password=st.secrets["password"], host='aact-db.ctti-clinicaltrials.org', port='5432')
 df = pd.read_sql_query("select count(*) from studies", con=conn)
 
-print(df)
+st.write(df)
 
