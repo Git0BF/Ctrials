c29f1171cf34fb316901421b997f18c266b9db2d
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-06-16 12:10:11 +0200

    Update Ctrials.py

--- Ctrials.py
+++ Ctrials.py
@@ -1,9 +1,15 @@
-import psycopg2
+import pyodbc 
 import pandas as pd
 import streamlit as st
 
-conn = psycopg2.connect(dbname='aact', user='git0bf', password=st.secrets["password"], host='aact-db.ctti-clinicaltrials.org', port='5432')
-df = pd.read_sql_query("select count(*) from studies", con=conn)
+server = 'aact-db.ctti-clinicaltrials.org' 
+database = 'aact' 
+username = 'git0bf' 
+password =  st.secrets["password"]
+cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+
+df = pd.read_sql_query('select count(*) from studies', cnxn)
 
 st.write(df)
 
