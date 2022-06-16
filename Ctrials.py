f6ae4f564ec720788bc39a09120d2a0c3d95f42c
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-06-16 11:37:49 +0200

    Update Ctrials.py

--- Ctrials.py
+++ Ctrials.py
@@ -1,5 +1,6 @@
 import psycopg2
 import pandas as pd
+import streamlit as st
 
 conn = psycopg2.connect(dbname='aact', user='git0bf', password=st.secrets["password"], host='aact-db.ctti-clinicaltrials.org', port='5432')
 df = pd.read_sql_query("select count(*) from studies", con=conn)
